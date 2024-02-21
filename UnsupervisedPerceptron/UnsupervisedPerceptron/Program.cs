2e257f1046dac5acc16eaa0933390a56b9a112ad
Deak Daniel <83765724+deak-daniel@users.noreply.github.com>
2024-02-21 21:12:33 +0100

    Fixed SOm.

--- UnsupervisedPerceptron/UnsupervisedPerceptron/Program.cs
+++ UnsupervisedPerceptron/UnsupervisedPerceptron/Program.cs
@@ -8,27 +8,41 @@
             int NUMBER_OF_NEURONS = 10;
             int NUMBER_OF_TRAINING_CYCLES = 20;
 
+            int index = 0; List<KohonenNeuron> neurons = new List<KohonenNeuron>();
+
             double in1 = Math.Round(rnd.NextDouble(), 2);
             double in2 = Math.Round(rnd.NextDouble(), 2);
-
-            List<KohonenNeuron> neurons = new List<KohonenNeuron>();
             for (int i = 0; i < NUMBER_OF_NEURONS; i++) // initalizing neurons
             {
-                neurons.Add(new KohonenNeuron(in1,in2, i)); 
+                neurons.Add(new KohonenNeuron(in1, in2, i));
+
             }
 
-            Console.WriteLine($"Desired Value: {neurons[0].InputVectorLength}, Input1: {in1}, Input2: {in2}");
-
-            double minDeg = neurons.Min(y => y.DegreeOfInputVector_and_WeightVector); // selecting the vector closest (smallest degree value between it and the input vector)
-                                                                                      // to the input vector
-            KohonenNeuron winnerNeuron = neurons[neurons.IndexOf(neurons.FirstOrDefault(x => x.DegreeOfInputVector_and_WeightVector == minDeg))];
-
-            for (int i = 0; i < NUMBER_OF_TRAINING_CYCLES; i++) // running training loop
+            while (index < NUMBER_OF_TRAINING_CYCLES)
             {
+                in1 = Math.Round(rnd.NextDouble(), 2);
+                in2 = Math.Round(rnd.NextDouble(), 2);
+                for (int i = 0; i < neurons.Count; i++)
+                {
+                    neurons[i].Initialize(in1, in2);
+                    neurons[i].StartEval();
+                }
+
+
+
+                Console.WriteLine($"Desired Value: {neurons[0].InputVectorLength}, Input1: {in1}, Input2: {in2}");
+
+                double minDeg = neurons.Max(y => y.Output); // selecting the vector closest (smallest degree value between it and the input vector)
+                                                            // to the input vector
+                KohonenNeuron winnerNeuron = neurons[neurons.IndexOf(neurons.FirstOrDefault(x => x.Output == minDeg))];
+
+
                 winnerNeuron.TrainWeights();
-                winnerNeuron.StartEval();
-                Console.WriteLine($"\tNeuron: {winnerNeuron.ID} output: {winnerNeuron.WeightVectorLength}, weight_1: {winnerNeuron.Weight_1}, weight_2: {winnerNeuron.Weight_2} Degree between InputVector: {winnerNeuron.DegreeOfInputVector_and_WeightVector}°");
+                Console.WriteLine($"\tNeuron: {winnerNeuron.ID} output: {winnerNeuron.WeightVectorLength}, weight_1: {winnerNeuron.Weight_1}, weight_2: {winnerNeuron.Weight_2} ");
+
+                index++;
             }
+            Console.ReadKey();
         }
     }
     public class KohonenNeuron
@@ -37,7 +51,7 @@
         public double Weight_1 { get; private set; }
         public double Weight_2 { get; private set; }
         private double Input_1;
-        private double Input_2; 
+        private double Input_2;
         private double Normalized_Input_1;
         private double Normalized_Input_2;
         private double Normalized_Weight_1;
@@ -61,7 +75,7 @@
             this.Weight_1 = rnd.NextDouble() * 2 - 1; // initializing random values to the weights
             this.Weight_2 = rnd.NextDouble() * 2 - 1;
         }
-        public void StartEval(double? input1 = null, double? input2 = null)
+        public void Initialize(double? input1 = null, double? input2 = null)
         {
             if (input1 != null && input2 != null) // if there are new input values, then we re-calculate the length, and then re-normalize the vectors
             {
@@ -72,9 +86,9 @@
                 this.Normalized_Input_1 = Math.Round(Input_1 / InputVectorLength, 4);
                 this.Normalized_Input_2 = Math.Round(Input_2 / InputVectorLength, 4);
             }
-
-            
-
+        }
+        public void StartEval()
+        {
             this.Weight_1 = Math.Round(Weight_1, 2); // rounding weights down to 2 deciamls
             this.Weight_2 = Math.Round(Weight_2, 2);
             this.WeightVectorLength = Math.Round(Math.Sqrt(Math.Pow((double)Weight_1, 2) + Math.Pow((double)Weight_2, 2)), 4); // calculating the length of the weight vector(s)
@@ -83,7 +97,7 @@
             this.Normalized_Weight_2 = Math.Round((double)Weight_2 / WeightVectorLength, 4);
 
 
-            this.Output = (this.Normalized_Input_1 * this.Normalized_Weight_1) + (this.Normalized_Input_2 * this.Normalized_Weight_2);
+            this.Output = Math.Round((this.Normalized_Input_1 * this.Normalized_Weight_1) + (this.Normalized_Input_2 * this.Normalized_Weight_2), 2);
 
             GetDegree();
         }
